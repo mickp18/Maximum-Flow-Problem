d71ab5e659284e8c9795c83104af7311f1cec7ee
mickp18 <miperseo@gmail.com>
2024-12-22 21:32:30 +0000

    add script for automating process Co-authored-by: Nikolova  Mina Pavlinova <s333972@studenti.polito.it> Co-authored-by: Giorgio Silvestre <GioSilve@users.noreply.github.com>

---
+++ run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+# Get the directory where the script is located
+SCRIPT_DIR=$(dirname $(realpath ${BASH_SOURCE[0]}))
+
+# Get the paths of the program & the exe
+SOLVER=$SCRIPT_DIR/MaxFlowSolver.hpp
+EDGE=$SCRIPT_DIR/Edge.hpp
+MAIN=$SCRIPT_DIR/main.cpp
+PROG=$SCRIPT_DIR/MaxFlowSolver
+
+
+# Compile the program
+g++ $MAIN $SOLVER $EDGE  -o $PROG
+
+# Run the program
+$PROG
+
+
+# Create graph with Graphviz
+
+g++ -o graphviz  graphViz.cpp  -lgvc -lcgraph
+
+./graphviz
+
+# xdg-open graph.png
+
+
+
