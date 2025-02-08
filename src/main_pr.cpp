23e57fe11fc591aff0c649a7d0d85e131ca508f8
mickp18 <miperseo@gmail.com>
2025-02-08 22:22:34 +0000

    add compute average execution time
    Co-authored-by: Giorgio Silvestre <GioSilve@users.noreply.github.com>

--- src/main_pr.cpp
+++ src/main_pr.cpp
@@ -14,18 +14,25 @@ int main(int argc, char *argv[])
         cout << "Please provide the path to the input file and the output file." << endl;
         return 1;
     }
-    
-    MaxFlowSolverParallel solver = MaxFlowSolverParallel(argv[1]);
-    high_resolution_clock::time_point t1 = high_resolution_clock::now();
-    //chrono::high_resolution_clock::time_point t1 = chrono::high_resolution_clock::now();
-    solver.solve();
-    high_resolution_clock::time_point t2 = high_resolution_clock::now();
-    //chrono::high_resolution_clock::time_point t2 = chrono::high_resolution_clock::now();
-    auto duration = duration_cast<microseconds>(t2 - t1);
+    auto tot_time = 0;
+    for (int i = 0; i < 5; i++) {
+        MaxFlowSolverParallel solver = MaxFlowSolverParallel(argv[1]);
+        high_resolution_clock::time_point t1 = high_resolution_clock::now();
+        // chrono::high_resolution_clock::time_point t1 = chrono::high_resolution_clock::now();
+        solver.solve();
+        high_resolution_clock::time_point t2 = high_resolution_clock::now();
+        // chrono::high_resolution_clock::time_point t2 = chrono::high_resolution_clock::now();
+        auto duration = duration_cast<microseconds>(t2 - t1);
 
-    cout << "Max flow: " << solver.getMaxFlow() << endl;
-    cout << "found in: " <<  duration.count() << "ns" << endl;
-    fout << "found in: " <<  duration.count() << "ns" << endl;
+        cout << "Max flow: " << solver.getMaxFlow() << endl;
+        cout << "found in: " << duration.count() << " micros" << endl;
+        fout << "found in: " << duration.count() << " micros" << endl;
+        cout << "\n";
+        tot_time += duration.count();
+    }
+
+    cout << "\nAverage time: " << tot_time / 5 << " micros" << endl;
+    
 
     // solver.printGraphToFile(argv[2]);
     
